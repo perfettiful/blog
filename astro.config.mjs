13361df03a197382bdc84b484a54eab8800703aa
Nathan Perfetti <nathanperfetti@gmail.com>
2026-01-18 13:35:10 -0600

    blog structure and posts

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://blog.perfettiful.dev',
+  integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
